cd8b594400c4380e96100ac3435e1c28f9f4bbe9
cherub randhawa <cherubs1897@gmail.com>
2024-05-08 02:18:07 -0400

    Added footer section and typing animation

--- Script/script.js
+++ Script/script.js
@@ -9,3 +9,33 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+
+
+// Typing effect function
+function typeWriter(elementId, text, speed) {
+    const element = document.getElementById(elementId);
+    let i = 0;
+
+    function type() {
+        if (i < text.length) {
+            element.textContent += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        }
+    }
+
+    type();
+}
+
+// Call the function to start typing effect when the page is loaded
+window.onload = function() {
+    // Element ID and text to type
+    const elementId = 'dynamicText';
+    const text = "I am an Early Software Professional based out of Toronto with experience in Software Development and Testing.";
+
+    // Typing speed (milliseconds)
+    const speed = 40;
+
+    // Start typing effect
+    typeWriter(elementId, text, speed);
+};
